940fb081217e0ad13c46042e3fd837e24a97f412
openshelf-talha <talhakhandir4@gmail.com>
2026-08-14 20:54:13 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='openshelf-v5-static';
-const ASSETS=['./','./index.html','./app.html','./about.html','./submit.html','./styles.css','./apps.js','./site.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html','./app.html','./about.html','./styles.css','./apps.js','./site.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
